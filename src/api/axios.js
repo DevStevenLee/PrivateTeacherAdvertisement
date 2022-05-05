81f24329a3c85b4acda0de7cbabc7b35eea19cd4
Steven Lee <mr938363@gmail.com>
2022-05-05 16:40:52 +0900

    Feat($auth): Add refresh token
    Save refresh token into localstorage and context

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,12 +2,12 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const instance = axios.create({
-    baseURL: "https://743c-203-252-106-49.ngrok.io"
+    baseURL: "https://1502-14-45-158-21.ngrok.io"
 });
 
 instance.interceptors.request.use(
     async (config) => {
-        const token = await AsyncStorage.getItem('token');
+        const token = await AsyncStorage.getItem('access_token');
         if(token){
             config.headers.Authorization = `Bearer ${token}`;
         }
