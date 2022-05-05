c8377f3cdfbeb158ac74c27f97d7cbbea4c74fe3
Uijin Lee <50033459+DevStevenLee@users.noreply.github.com>
2022-05-05 21:33:24 +0900

    Revert "Update bunch of features"

--- src/components/CreateProfile/profile_info/CreateDropdown.js
+++ src/components/CreateProfile/profile_info/CreateDropdown.js
@@ -67,7 +67,6 @@ const CreateDropdown = ({
             }}
 
             animated={ true }
-            saveScrollPosition={ false }
 
             onSelect={(index) => {
                 setTextColor("black");
